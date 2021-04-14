9656dd100d38ea2145ff9e0cd5df2cc0474292f5
jerry2204 <jerryandrianto22@gmail.com>
2021-04-14 21:41:46 +0700

    create animation

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ new TypeIt('#type1', {
     loop: true,
     waitUntilVisible: true,
 }).type("Designer", { delay: 400 })
-.pause(500).delete(9).type("Developer", { delay: 400 })
-.pause(500).delete(9).go();
+.pause(500).delete(9).type("Web Developer", { delay: 400 })
+.pause(500).delete(14).type("Mobile Developer", { delay: 400 })
+.pause(500).delete(17).go();
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: .5, y: -10 })
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 })
+gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.25, x: -200 })
+gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 1.5, y: -50 })
+gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 1.75, y: -45 })
+gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 2, y: -30 })
+gsap.from(".hero a", { opacity: 0, duration: 1, delay: 2.25, y: 50 })
+gsap.from(".nav-item", { opacity: 0, duration: .5, delay: 1.2, y: 30, stagger: 0.2 })
+gsap.from(".icons", { opacity: 0, duration: 1, delay: 2.25, y: -50 })
